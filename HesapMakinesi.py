fdf0a85fa4917b509cc97b64429b72244e5ab525
muhammed267 <talhaaba456@gmail.com>
2021-02-06 13:06:24 +0300

    added HesapMakinesi

---
+++ HesapMakinesi.py
@@ -0,0 +1,12 @@
+#Hesap Makinesi
+print("***************Hosgeldiniz**************")
+print("Lütfen islem yapmak istediğiniz iki sayiyi giriniz: ")
+num1 = float(input("Ilk sayiyi giriniz: "))
+num2 = float(input("Ikinci sayiyi giriniz: "))
+print("=====================================")
+print("Sayilarin Toplami : ",num1+num2)
+print("Sayilarin Cıkarimi: ",num1-num2)
+print("Sayilarin Carpimi : ",num1*num2)
+print("Sayilarin Bolumu  : ",num1/num2) 
+print("=====================================")
+
